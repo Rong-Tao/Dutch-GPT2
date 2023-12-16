cc1b28c279ac5857b2217e019564432b68920420
Rong-Tao <trsjtu17@gmail.com>
2023-12-16 11:43:11 +0800

    gpt2-small finished

---
+++ training/GPT2/dataset.py
@@ -0,0 +1,36 @@
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data.distributed import DistributedSampler
+from transformers import BertTokenizerFast
+import datasets
+# dataset.py
+
+class GPT2Dataset(Dataset):
+    def __init__(self):
+        self.ds  = datasets.Dataset.load_from_disk("../../dataset/Tokenized_dataset.hf")
+
+    def __len__(self):
+        """Returns the number of examples in the dataset."""
+        return len(self.ds)
+
+    def __getitem__(self, idx):
+        input_id = self.ds[idx]['input_ids']
+        attention_mask = self.ds[idx]['attention_mask']
+        targets = input_id[1:]
+        targets = torch.cat([targets, torch.tensor([1])], dim=0)
+        return input_id, attention_mask, targets
+
+
+def get_loaders(world_size, rank, batch_size, split_ratio):
+    full_dataset = GPT2Dataset()
+    train_size = int(split_ratio * len(full_dataset))
+    validation_size = len(full_dataset) - train_size
+    train_dataset, validation_dataset = random_split(full_dataset, [train_size, validation_size])
+
+    train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler)
+
+    validation_sampler = DistributedSampler(validation_dataset, num_replicas=world_size, rank=rank)
+    validation_loader = DataLoader(validation_dataset, batch_size=batch_size, sampler=validation_sampler)
+
+    return train_loader, validation_loader
---
+++ training/GPT2/util.py
@@ -0,0 +1,64 @@
+import os
+import argparse
+import torch
+import torch.optim as optim
+
+BATCH_SIZE = 20
+EPOCH_NUM = 50
+TRAIN_VAL_RATIO = 0.95 # 0-1
+
+criterion = torch.nn.CrossEntropyLoss(ignore_index=1)
+
+def get_optimizer(model):
+    optimizer = optim.AdamW(model.module.parameters(), lr=1e-4)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=EPOCH_NUM)
+
+    return optimizer, scheduler
+
+def batch_logger(model, writer, batch_idx, step_num, loss, tokenizer):
+    writer.add_scalar('Batch Training Loss', loss, step_num)
+    generate_text(step_num, model, writer, tokenizer, max_length=100)
+
+
+
+def generate_text(step_num, model, writer, tokenizer, max_length=100):
+    model.eval()
+
+    input_ids = tokenizer.encode("<s>Kerstmis komt dichterbij en mijn lieve vrienden in Eindhoven", return_tensors="pt").to(model.device)
+
+    output_sequences = model.generate(
+        input_ids=input_ids,
+        max_length=max_length,
+        pad_token_id=1,
+        eos_token_id=2
+    )
+
+    if output_sequences.is_cuda:
+        output_sequences = output_sequences.cpu()
+
+    generated_text = tokenizer.decode(output_sequences[0], skip_special_tokens=True)
+    
+    writer.add_text('Generated Text', generated_text, step_num)
+
+
+
+def epoch_logger_saver(model, writer, epoch, mean_trainloss, validation_loss, best_loss, state_dict_dir):
+    writer.add_scalar('Epoch Training Loss', mean_trainloss, epoch)
+    writer.add_scalar('Epoch Validation Loss', validation_loss, epoch)
+    if mean_trainloss < best_loss:
+        best_loss = mean_trainloss
+        model_save_path = os.path.join(state_dict_dir, f"epoch_{epoch}.pth")
+        torch.save(model.state_dict(), model_save_path)
+    return best_loss
+
+def arg():
+    parser = argparse.ArgumentParser(description='Pass log directories to main script.') 
+    parser.add_argument('--output_log', type=str, help='Path to the output log directory.')
+    args = parser.parse_args()
+    result_dir = args.output_log 
+    tensor_bd_dir = os.path.join(result_dir, 'tensorboard')
+    state_dict_dir = os.path.join(result_dir, 'state_dict') 
+    os.makedirs(result_dir, exist_ok=True) 
+    os.makedirs(tensor_bd_dir, exist_ok=True)
+    os.makedirs(state_dict_dir, exist_ok=True)
+    return result_dir, state_dict_dir, tensor_bd_dir
